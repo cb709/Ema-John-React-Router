c9e369f31d90401165a80e8979c1907650fbfe5d
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-10-25 18:35:32 +0600

    added navigation toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/Logo.svg";
 import "./Header.css";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Header = () => {
-    const handleDropdown = () => {
-        let target  = document.getElementById('nav-link')
-        
-        if(target.style.visibility === 'hidden'){
-            target.style.visibility ='visible';
-        }else {
-            target.style.visibility ='hidden';
-        }
+  const [close, setClose] = useState(false);
+  const handleDropdown = () => {
+    let target = document.getElementById("nav-link");
+    console.log(target.style.top);
+    if (target.style.top === "" || target.style.top === "-100%") {
+      target.style.visibility = "visible";
+      target.style.top = "80px";
+      setClose(!close);
+    } else {
+      target.style.visibility = "hidden";
+      target.style.top = "-100%";
+      setClose(!close);
     }
+    console.log(target.style.top);
+  };
   return (
     <nav className="header">
       <div className="logo">
@@ -24,7 +30,11 @@ const Header = () => {
       </div>
       <div className="dropdown-menu">
         <button onClick={handleDropdown}>
-          <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+          {close ? (
+            <FontAwesomeIcon className="close-icon" icon={faClose}></FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon className="menu-icon" icon={faBars}></FontAwesomeIcon>
+          )}
         </button>
       </div>
       <div id="nav-link" className="nav-link">
